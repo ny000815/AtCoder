d5e9f45b35835928c4fcf2997f89da6252e07b7d
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-04-07 16:06:12 +0000

    Auto backup: Tue Apr  7 16:06:12 UTC 2026

---
+++ submissions/dp/dp_c/74730395.py
@@ -0,0 +1,10 @@
+N = int(input())
+a = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * 3 for _ in range(N+1)]
+for i in range(N):
+  for j in range(3):
+    for k in range(3):
+      if j == k:
+        continue
+      dp[i+1][k] = max(dp[i+1][k], dp[i][j] + a[i][k])
+print(max(dp[N]))
