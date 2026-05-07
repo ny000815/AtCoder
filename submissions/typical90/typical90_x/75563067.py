dccb4c0be4d307164cd8153c2404ab6aa185dc3f
ny000815 <66006097+ny000815@users.noreply.github.com>
2026-05-07 07:40:04 +0000

    Auto backup: Thu May  7 07:40:04 UTC 2026

---
+++ submissions/typical90/typical90_x/75563067.py
@@ -0,0 +1,7 @@
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+cnt = 0
+for i in range(N):
+  cnt += abs(A[i] - B[i])
+print("Yes" if (K-cnt)%2 == 0 and K >= cnt else "No")
